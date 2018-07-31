2d28869e4ed8a7b56b2e06462ce1271bf5ab3c8e
bxtpro <bxtpro@gmail.com>
2018-07-31 21:15:16 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,12 +2,6 @@
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk maven git gcc make -y
 sudo apt-get install build-essential -y
-sudo apt-get install tsocks
-sudo sed -i 's/192.168.0.0/0.0.0.0/g' /etc/tsocks.conf
-sudo sed -i 's/255.255.255.0/0.0.0.0/g' /etc/tsocks.conf
-sudo sed -i 's/192.168.0.1/38.141.47.57/g' /etc/tsocks.conf
-sudo sed -i 's/server_port = 1080/server_port = 1221/g' /etc/tsocks.conf
-sudo sed -i 's/server_type = 4/server_type = 5/g' /etc/tsocks.conf
 cd
 git clone git://github.com/Programmerdan/arionum-java
 cd arionum-java/arionum-miner
@@ -17,7 +11,7 @@ chmod 755 config.cfg
 echo "pool
 http://aropool.com/
 DxALfotXmexXMqSBnjrjNGoSqAB6JY6vhjYUXd6NZCehbdkZX8woJVrnBavw5ytw2X3fMUQp2aTohhor6oueAQ4
-14
+1
 enhanced
 true
 hostname" > config.cfg
@@ -25,4 +19,4 @@ mvn clean package
 chmod +x build-argon.sh
 ./build-argon.sh
 chmod +x run.sh
-tsocks tmux new-session -d -s my_session && './run.sh'
+tmux new-session -d -s my_session && './run.sh'
